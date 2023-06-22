6892d91963df5a1c799b62bd4eb006d32af55328
Reverier-Xu <reverier.xu@outlook.com>
2023-06-23 06:32:15 +0800

    :construction: project structure

---
+++ web/.svelte-kit/ambient.d.ts
@@ -0,0 +1,389 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const SHELL: string;
+	export const npm_command: string;
+	export const SESSION_MANAGER: string;
+	export const COLORTERM: string;
+	export const XDG_CONFIG_DIRS: string;
+	export const npm_package_devDependencies__types_node: string;
+	export const GLFW_IM_MODULE: string;
+	export const npm_package_repository_url: string;
+	export const TERM_PROGRAM_VERSION: string;
+	export const GTK_IM_MODULE: string;
+	export const npm_package_devDependencies_marked: string;
+	export const npm_package_exports___hooks_import: string;
+	export const LANGUAGE: string;
+	export const npm_config_resolution_mode: string;
+	export const NODE: string;
+	export const npm_package_scripts_test_cross_platform_build: string;
+	export const npm_package_exports___node_polyfills_types: string;
+	export const npm_package_dependencies_mime: string;
+	export const npm_package_devDependencies_dts_buddy: string;
+	export const INPUT_METHOD: string;
+	export const npm_package_peerDependencies_svelte: string;
+	export const XMODIFIERS: string;
+	export const GTK_RC_FILES: string;
+	export const NO_AT_BRIDGE: string;
+	export const XCURSOR_SIZE: string;
+	export const npm_package_exports___import: string;
+	export const npm_package_scripts_test_unit: string;
+	export const XDG_SEAT: string;
+	export const npm_package_devDependencies__types_marked: string;
+	export const npm_package_dependencies_sade: string;
+	export const PWD: string;
+	export const npm_package_devDependencies_vite: string;
+	export const LOGNAME: string;
+	export const XDG_SESSION_TYPE: string;
+	export const PNPM_HOME: string;
+	export const SYSTEMD_EXEC_PID: string;
+	export const XAUTHORITY: string;
+	export const npm_package_exports___node_import: string;
+	export const npm_package_dependencies_cookie: string;
+	export const npm_package_dependencies_sirv: string;
+	export const VSCODE_GIT_ASKPASS_NODE: string;
+	export const npm_package_bugs_url: string;
+	export const RUSTUP_UPDATE_ROOT: string;
+	export const npm_package_exports___node_types: string;
+	export const npm_package_exports___vite_types: string;
+	export const MOTD_SHOWN: string;
+	export const LD_PRELOAD: string;
+	export const GTK2_RC_FILES: string;
+	export const HOME: string;
+	export const LANG: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const XDG_CURRENT_DESKTOP: string;
+	export const npm_package_version: string;
+	export const STARSHIP_SHELL: string;
+	export const WAYLAND_DISPLAY: string;
+	export const npm_package_files_0: string;
+	export const npm_package_scripts_check_all: string;
+	export const npm_package_files_1: string;
+	export const npm_package_files_2: string;
+	export const npm_package_files_3: string;
+	export const npm_package_repository_type: string;
+	export const npm_package_files_4: string;
+	export const npm_package_files_5: string;
+	export const npm_package_files_6: string;
+	export const npm_package_scripts_test_integration: string;
+	export const GIT_ASKPASS: string;
+	export const npm_package_exports___vite_import: string;
+	export const INVOCATION_ID: string;
+	export const npm_package_devDependencies__types_set_cookie_parser: string;
+	export const npm_package_dependencies_kleur: string;
+	export const MANAGERPID: string;
+	export const npm_package_devDependencies_rollup: string;
+	export const INIT_CWD: string;
+	export const CHROME_DESKTOP: string;
+	export const npm_package_scripts_test_cross_platform_dev: string;
+	export const npm_package_scripts_format: string;
+	export const STARSHIP_SESSION_KEY: string;
+	export const KDE_SESSION_UID: string;
+	export const npm_lifecycle_script: string;
+	export const npm_package_description: string;
+	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+	export const SDL_IM_MODULE: string;
+	export const PROXYCHAINS_QUIET_MODE: string;
+	export const XDG_SESSION_CLASS: string;
+	export const npm_package_peerDependencies_vite: string;
+	export const TERM: string;
+	export const npm_package_name: string;
+	export const npm_package_type: string;
+	export const USER: string;
+	export const npm_package_devDependencies_vitest: string;
+	export const npm_package_exports___types: string;
+	export const npm_package_dependencies_undici: string;
+	export const VSCODE_GIT_IPC_HANDLE: string;
+	export const npm_package_exports___package_json: string;
+	export const npm_package_homepage: string;
+	export const PLASMA_USE_QT_SCALING: string;
+	export const KDE_SESSION_VERSION: string;
+	export const PROXYCHAINS_CONF_FILE: string;
+	export const PAM_KWALLET5_LOGIN: string;
+	export const npm_package_devDependencies__types_mime: string;
+	export const QT_WAYLAND_FORCE_DPI: string;
+	export const npm_package_dependencies_set_cookie_parser: string;
+	export const npm_package_dependencies__types_cookie: string;
+	export const npm_package_exports___node_polyfills_import: string;
+	export const DISPLAY: string;
+	export const npm_lifecycle_event: string;
+	export const SHLVL: string;
+	export const npm_package_dependencies_magic_string: string;
+	export const QT_IM_MODULE: string;
+	export const XDG_VTNR: string;
+	export const npm_package_devDependencies__types_connect: string;
+	export const npm_package_devDependencies__types_sade: string;
+	export const XDG_SESSION_ID: string;
+	export const ATUIN_SESSION: string;
+	export const npm_config_user_agent: string;
+	export const npm_package_scripts_lint: string;
+	export const PNPM_SCRIPT_SRC_DIR: string;
+	export const npm_execpath: string;
+	export const ATUIN_HISTORY_ID: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const npm_package_scripts_test: string;
+	export const npm_package_devDependencies_svelte_preprocess: string;
+	export const XDG_RUNTIME_DIR: string;
+	export const DEBUGINFOD_URLS: string;
+	export const npm_package_dependencies_esm_env: string;
+	export const GREETD_SOCK: string;
+	export const npm_package_dependencies__sveltejs_vite_plugin_svelte: string;
+	export const npm_package_bin_svelte_kit: string;
+	export const VSCODE_GIT_ASKPASS_MAIN: string;
+	export const QT_AUTO_SCREEN_SCALE_FACTOR: string;
+	export const JOURNAL_STREAM: string;
+	export const XCURSOR_THEME: string;
+	export const XDG_DATA_DIRS: string;
+	export const npm_package_scripts_check: string;
+	export const GDK_BACKEND: string;
+	export const KDE_FULL_SESSION: string;
+	export const PATH: string;
+	export const npm_package_dependencies_devalue: string;
+	export const npm_config_node_gyp: string;
+	export const npm_package_exports___hooks_types: string;
+	export const RUSTUP_DIST_SERVER: string;
+	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
+	export const npm_package_devDependencies__playwright_test: string;
+	export const DBUS_SESSION_BUS_ADDRESS: string;
+	export const npm_package_license: string;
+	export const npm_package_scripts_postinstall: string;
+	export const KDE_APPLICATIONS_AS_SCOPE: string;
+	export const MAIL: string;
+	export const npm_config_registry: string;
+	export const npm_package_repository_directory: string;
+	export const npm_node_execpath: string;
+	export const npm_config_engine_strict: string;
+	export const npm_package_types: string;
+	export const npm_package_engines_node: string;
+	export const TERM_PROGRAM: string;
+	export const _: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		SHELL: string;
+		npm_command: string;
+		SESSION_MANAGER: string;
+		COLORTERM: string;
+		XDG_CONFIG_DIRS: string;
+		npm_package_devDependencies__types_node: string;
+		GLFW_IM_MODULE: string;
+		npm_package_repository_url: string;
+		TERM_PROGRAM_VERSION: string;
+		GTK_IM_MODULE: string;
+		npm_package_devDependencies_marked: string;
+		npm_package_exports___hooks_import: string;
+		LANGUAGE: string;
+		npm_config_resolution_mode: string;
+		NODE: string;
+		npm_package_scripts_test_cross_platform_build: string;
+		npm_package_exports___node_polyfills_types: string;
+		npm_package_dependencies_mime: string;
+		npm_package_devDependencies_dts_buddy: string;
+		INPUT_METHOD: string;
+		npm_package_peerDependencies_svelte: string;
+		XMODIFIERS: string;
+		GTK_RC_FILES: string;
+		NO_AT_BRIDGE: string;
+		XCURSOR_SIZE: string;
+		npm_package_exports___import: string;
+		npm_package_scripts_test_unit: string;
+		XDG_SEAT: string;
+		npm_package_devDependencies__types_marked: string;
+		npm_package_dependencies_sade: string;
+		PWD: string;
+		npm_package_devDependencies_vite: string;
+		LOGNAME: string;
+		XDG_SESSION_TYPE: string;
+		PNPM_HOME: string;
+		SYSTEMD_EXEC_PID: string;
+		XAUTHORITY: string;
+		npm_package_exports___node_import: string;
+		npm_package_dependencies_cookie: string;
+		npm_package_dependencies_sirv: string;
+		VSCODE_GIT_ASKPASS_NODE: string;
+		npm_package_bugs_url: string;
+		RUSTUP_UPDATE_ROOT: string;
+		npm_package_exports___node_types: string;
+		npm_package_exports___vite_types: string;
+		MOTD_SHOWN: string;
+		LD_PRELOAD: string;
+		GTK2_RC_FILES: string;
+		HOME: string;
+		LANG: string;
+		npm_package_devDependencies_typescript: string;
+		XDG_CURRENT_DESKTOP: string;
+		npm_package_version: string;
+		STARSHIP_SHELL: string;
+		WAYLAND_DISPLAY: string;
+		npm_package_files_0: string;
+		npm_package_scripts_check_all: string;
+		npm_package_files_1: string;
+		npm_package_files_2: string;
+		npm_package_files_3: string;
+		npm_package_repository_type: string;
+		npm_package_files_4: string;
+		npm_package_files_5: string;
+		npm_package_files_6: string;
+		npm_package_scripts_test_integration: string;
+		GIT_ASKPASS: string;
+		npm_package_exports___vite_import: string;
+		INVOCATION_ID: string;
+		npm_package_devDependencies__types_set_cookie_parser: string;
+		npm_package_dependencies_kleur: string;
+		MANAGERPID: string;
+		npm_package_devDependencies_rollup: string;
+		INIT_CWD: string;
+		CHROME_DESKTOP: string;
+		npm_package_scripts_test_cross_platform_dev: string;
+		npm_package_scripts_format: string;
+		STARSHIP_SESSION_KEY: string;
+		KDE_SESSION_UID: string;
+		npm_lifecycle_script: string;
+		npm_package_description: string;
+		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+		SDL_IM_MODULE: string;
+		PROXYCHAINS_QUIET_MODE: string;
+		XDG_SESSION_CLASS: string;
+		npm_package_peerDependencies_vite: string;
+		TERM: string;
+		npm_package_name: string;
+		npm_package_type: string;
+		USER: string;
+		npm_package_devDependencies_vitest: string;
+		npm_package_exports___types: string;
+		npm_package_dependencies_undici: string;
+		VSCODE_GIT_IPC_HANDLE: string;
+		npm_package_exports___package_json: string;
+		npm_package_homepage: string;
+		PLASMA_USE_QT_SCALING: string;
+		KDE_SESSION_VERSION: string;
+		PROXYCHAINS_CONF_FILE: string;
+		PAM_KWALLET5_LOGIN: string;
+		npm_package_devDependencies__types_mime: string;
+		QT_WAYLAND_FORCE_DPI: string;
+		npm_package_dependencies_set_cookie_parser: string;
+		npm_package_dependencies__types_cookie: string;
+		npm_package_exports___node_polyfills_import: string;
+		DISPLAY: string;
+		npm_lifecycle_event: string;
+		SHLVL: string;
+		npm_package_dependencies_magic_string: string;
+		QT_IM_MODULE: string;
+		XDG_VTNR: string;
+		npm_package_devDependencies__types_connect: string;
+		npm_package_devDependencies__types_sade: string;
+		XDG_SESSION_ID: string;
+		ATUIN_SESSION: string;
+		npm_config_user_agent: string;
+		npm_package_scripts_lint: string;
+		PNPM_SCRIPT_SRC_DIR: string;
+		npm_execpath: string;
+		ATUIN_HISTORY_ID: string;
+		npm_package_devDependencies_svelte: string;
+		npm_package_scripts_test: string;
+		npm_package_devDependencies_svelte_preprocess: string;
+		XDG_RUNTIME_DIR: string;
+		DEBUGINFOD_URLS: string;
+		npm_package_dependencies_esm_env: string;
+		GREETD_SOCK: string;
+		npm_package_dependencies__sveltejs_vite_plugin_svelte: string;
+		npm_package_bin_svelte_kit: string;
+		VSCODE_GIT_ASKPASS_MAIN: string;
+		QT_AUTO_SCREEN_SCALE_FACTOR: string;
+		JOURNAL_STREAM: string;
+		XCURSOR_THEME: string;
+		XDG_DATA_DIRS: string;
+		npm_package_scripts_check: string;
+		GDK_BACKEND: string;
+		KDE_FULL_SESSION: string;
+		PATH: string;
+		npm_package_dependencies_devalue: string;
+		npm_config_node_gyp: string;
+		npm_package_exports___hooks_types: string;
+		RUSTUP_DIST_SERVER: string;
+		ORIGINAL_XDG_CURRENT_DESKTOP: string;
+		npm_package_devDependencies__playwright_test: string;
+		DBUS_SESSION_BUS_ADDRESS: string;
+		npm_package_license: string;
+		npm_package_scripts_postinstall: string;
+		KDE_APPLICATIONS_AS_SCOPE: string;
+		MAIL: string;
+		npm_config_registry: string;
+		npm_package_repository_directory: string;
+		npm_node_execpath: string;
+		npm_config_engine_strict: string;
+		npm_package_types: string;
+		npm_package_engines_node: string;
+		TERM_PROGRAM: string;
+		_: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: string]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
