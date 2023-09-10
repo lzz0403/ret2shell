b3b6abddec9f2bb34c13db4d405407a373359903
Reverier-Xu <reverier.xu@outlook.com>
2023-09-10 22:17:03 +0800

    :construction: wiki page start

---
+++ web/src/routes/wiki/+layout.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import RxButton from '$lib/components/RxButton.svelte'
+  import { fly } from 'svelte/transition'
+  import { quintOut } from 'svelte/easing'
+
+  let toggleSidebar = false
+  let screenWidth: number
+
+  $: showSidebar = screenWidth > 1024 // lg
+</script>
+
+<svelte:window bind:innerWidth={screenWidth} />
+
+<div class="flex-1 flex flex-row">
+  {#if showSidebar}
+    <div class="w-1/4 min-w-[400px] max-w-[512px] bg-base-100/40 backdrop-blur border-r border-r-base-content/5" />
+  {:else}
+    <RxButton
+      size="lg"
+      class="fixed right-6 bottom-6"
+      on:click={() => {
+        toggleSidebar = !toggleSidebar
+      }}
+    >
+      <span class="icon-[fluent--navigation-16-regular] w-5 h-5" />
+    </RxButton>
+  {/if}
+  <div class="flex-1">
+    <slot />
+  </div>
+  {#if toggleSidebar && !showSidebar}
+    <div
+      class="fixed w-full max-w-[400px] h-full backdrop-blur-xl bg-base-100/40 border-r border-r-base-content/5"
+      transition:fly={{ delay: 100, duration: 300, x: -256, y: 0, opacity: 0, easing: quintOut }}
+    />
+    <RxButton
+      size="lg"
+      class="fixed right-6 bottom-6"
+      on:click={() => {
+        toggleSidebar = !toggleSidebar
+      }}
+    >
+      <span class="icon-[fluent--dismiss-16-regular] w-5 h-5" />
+    </RxButton>
+  {/if}
+</div>
