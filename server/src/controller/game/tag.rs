c3ab1e9f365a647288f59724d7973fdbe38262a3
dx3906 <21009201351@stu.xidian.edu.cn>
2023-09-13 21:30:07 +0800

    :sparkles: tag api

--- server/src/controller/game/tag.rs
+++ server/src/controller/game/tag.rs
@@ -1,6 +1,63 @@
-use crate::controller::GlobalState;
-use axum::Router;
+use crate::{
+    controller::{layer::auth::permission_required_all, GlobalState},
+    entity::{tag, user::Permission},
+};
+use axum::{
+    extract::{Path, State},
+    middleware,
+    response::IntoResponse,
+    routing::{delete, get, post},
+    Json, Router,
+};
+use hyper::StatusCode;
+use sea_orm::{DatabaseConnection, DbErr};
+use tracing::error;
 
 pub fn router(_state: &GlobalState) -> Router<GlobalState> {
     Router::new()
+        .route("/", post(create_tag))
+        .route("/:id", delete(delete_tag))
+        .route_layer(middleware::from_fn(permission_required_all!(
+            Permission::Organize
+        )))
+        .route("/", get(get_tag_list))
+}
+
+async fn get_tag_list(
+    State(ref conn): State<DatabaseConnection>,
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    match tag::get_tag_list(conn).await {
+        Ok(tags) => Ok(Json(tags)),
+        Err(err) => {
+            error!("get_tag_list error: {}", err);
+            Err((StatusCode::INTERNAL_SERVER_ERROR, "failed to get tag list"))
+        }
+    }
+}
+
+async fn create_tag(
+    State(ref conn): State<DatabaseConnection>,
+    Json(tag): Json<tag::Model>,
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    match tag::create_tag(conn, tag).await {
+        Ok(_) => Ok(StatusCode::CREATED),
+        Err(err) => {
+            error!("create_tag error: {}", err);
+            Err((StatusCode::INTERNAL_SERVER_ERROR, "failed to create tag"))
+        }
+    }
+}
+
+async fn delete_tag(
+    State(ref conn): State<DatabaseConnection>,
+    Path(id): Path<i64>,
+) -> Result<impl IntoResponse, (StatusCode, &'static str)> {
+    match tag::delete_tag(conn, id).await {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(DbErr::RecordNotFound(_)) => Err((StatusCode::NOT_FOUND, "tag not found")),
+        Err(err) => {
+            error!("delete_tag error: {}", err);
+            Err((StatusCode::INTERNAL_SERVER_ERROR, "failed to delete tag"))
+        }
+    }
 }
