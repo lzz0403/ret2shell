ab03ca7515bf4569529d752ca342581bfe140aa2
Reverier-Xu <reverier.xu@outlook.com>
2023-11-08 22:00:30 +0800

    :construction: repo router

--- server/src/controller/challenge/repo.rs
+++ server/src/controller/challenge/repo.rs
@@ -1,6 +1,18 @@
 use crate::controller::GlobalState;
 use axum::Router;
 
+/*
+ * Repo router
+ *
+ * user does not have any permissions, so this router if fully under admin layer.
+ *
+ * - repo entrypoint for wsvc sync tool (?)
+ * - web file list
+ * - web file content
+ * - web record list
+ * - web checkout
+ * - web upload file and record
+ */
 pub fn router(_state: &GlobalState) -> Router<GlobalState> {
     Router::new()
 }
