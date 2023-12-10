fbbcc37e19fce0555df5fc96c6905a432fb11517
Reverier-Xu <reverier.xu@outlook.com>
2023-12-10 22:15:29 +0800

    :arrow_up: import melt ui

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,18 +1,21 @@
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+import { preprocessMeltUI } from '@melt-ui/pp'
+import sequence from 'svelte-sequential-preprocessor'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [
+  preprocess: sequence([
     vitePreprocess(),
     preprocess({
       postcss: true,
       sass: true,
     }),
-  ],
+    preprocessMeltUI(),
+  ]),
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
