9bb651f18a3af3a700f112aa5f9a88394b76ec9e
Reverier-Xu <reverier.xu@woooo.tech>
2024-06-15 23:22:04 +0800

    :construction: add admin mask

--- web/src/lib/blocks/challenge/hammer.tsx
+++ web/src/lib/blocks/challenge/hammer.tsx
@@ -1,13 +1,18 @@
+import { Permission } from "@/lib/models/user";
+import { accountStore } from "@/lib/storage/account";
+import { gameStore } from "@/lib/storage/game";
 import xdsecMascotNormal from "@assets/imgs/xdsec-mascot-normal.webp";
 import xdsecMascotUnsee from "@assets/imgs/xdsec-mascot-unsee.webp";
+import { A } from "@solidjs/router";
 import { t } from "@storage/theme";
 import Button from "@widgets/button";
 import Card from "@widgets/card";
 import Input from "@widgets/input";
+import { Show } from "solid-js";
 
 export default function () {
     return (
-        <div class="flex flex-col min-h-full">
+        <div class="flex flex-col min-h-full relative">
             <div class="flex flex-col flex-1 p-3 lg:p-6 space-y-4">
                 <div class="self-start flex-row max-w-[calc(100%-4rem)] flex items-center">
                     <img src={xdsecMascotNormal} width={40} height={40} alt="ΦωΦ" />
@@ -34,6 +39,23 @@ export default function () {
                     }
                 />
             </div>
+            <Show
+                when={
+                    accountStore.id &&
+                    accountStore.permissions.includes(Permission.Game) &&
+                    gameStore.current?.admins.includes(accountStore.id)
+                }
+            >
+                <div class="absolute top-0 left-0 w-full h-full backdrop-blur flex items-center justify-center">
+                    <A
+                        class="font-bold hover:underline hover:text-primary flex items-center space-x-2"
+                        href={`/games/${gameStore.current?.id}/admin/hammers`}
+                    >
+                        <span class="icon-[fluent--open-20-regular] w-5 h-5" />
+                        <span>{t("game.admin.hammer.shouldGoto")}</span>
+                    </A>
+                </div>
+            </Show>
         </div>
     );
 }
