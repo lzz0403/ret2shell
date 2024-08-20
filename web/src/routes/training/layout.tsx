98d288e5ce3a31cb4aa008ec09f558b1768059c3
Reverier-Xu <reverier.xu@woooo.tech>
2024-08-20 10:05:15 +0800

    :sparkles: mobile narrow tips

--- web/src/routes/training/layout.tsx
+++ web/src/routes/training/layout.tsx
@@ -1,19 +1,41 @@
 import SidebarLayout from "@blocks/sidebar-layout";
+import { createBreakpoints } from "@solid-primitives/media";
 import { setGameStore } from "@storage/game";
 import { Title } from "@storage/header";
 import { platformStore } from "@storage/platform";
 import { t } from "@storage/theme";
-import { type JSX, onCleanup } from "solid-js";
+import Button from "@widgets/button";
+import { type JSX, Show, createSignal, onCleanup } from "solid-js";
+import { Transition } from "solid-transition-group";
 import SideBar from "./_blocks/sidebar";
 
 export default function (props: { children?: JSX.Element }) {
   onCleanup(() => {
     setGameStore({ current: null, games: [], preload: null });
   });
+  const breakpoints = {
+    lg: "1024px",
+  };
+  const matches = createBreakpoints(breakpoints);
+  const [showSidebar, setShowSidebar] = createSignal(false);
   return (
     <>
       <Title title={`${t("training.title")} - ${platformStore.config.name || t("platform.name")}`} />
-      <SidebarLayout leftBar={() => <SideBar />}>{props.children}</SidebarLayout>
+      <SidebarLayout leftBar={() => <SideBar />} showLeftBar={showSidebar()}>
+        {props.children}
+      </SidebarLayout>
+      <Transition name="slide-fade-right">
+        <Show when={!matches.lg}>
+          <Button class="fixed bottom-3 right-3" square onClick={() => setShowSidebar(!showSidebar())} type="button">
+            {/* icon-[fluent--navigation-20-regular] icon-[fluent--dismiss-20-regular] rotate-90 rotate-0 */}
+            <span
+              class={`transition-transform rotate-${showSidebar() ? "90" : "0"} icon-[fluent--${
+                showSidebar() ? "dismiss" : "navigation"
+              }-20-regular] w-5 h-5`}
+            />
+          </Button>
+        </Show>
+      </Transition>
     </>
   );
 }
