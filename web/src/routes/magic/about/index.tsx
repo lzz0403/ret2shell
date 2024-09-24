d1d27daa94d0ef8eda21fa37dd2058d10597b99e
Reverier-Xu <reverier.xu@stu.xidian.edu.cn>
2024-09-24 14:24:26 +0800

    :bug: fix about page on mobile

--- web/src/routes/magic/about/index.tsx
+++ web/src/routes/magic/about/index.tsx
@@ -1,8 +1,10 @@
 import LogoAnimate from "@assets/animates/logo-animate";
 import { Reverier } from "@assets/animates/Reverier";
 import { platformStore } from "@storage/platform";
+import { fullTheme } from "@storage/theme";
 import Divider from "@widgets/divider";
 import Tag from "@widgets/tag";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-solid";
 import { Show } from "solid-js";
 
 export default function() {
@@ -10,61 +12,74 @@ export default function() {
     <div class="flex-1 relative">
       <div class="absolute h-full w-full overflow-scroll snap-mandatory snap-y">
         <section class="h-full min-h-full snap-center flex flex-col items-center justify-center relative p-3">
-          <div class="max-w-3xl w-full flex flex-col items-center justify-center space-y-6">
-            <div class="flex flex-row space-x-6 items-center">
-              <LogoAnimate width={80} height={80} />
-              <div class="flex flex-col space-y-1">
-                <h2 class="text-3xl font-bold flex flex-row">
-                  <span class="text-primary">R</span>
-                  <span class="opacity-80">et</span>
-                  <span class="opacity-60">&nbsp;2&nbsp;</span>
-                  <span class="text-error">S</span>
-                  <span class="opacity-80">hell</span>
-                </h2>
-                <p class="text-base font-bold opacity-60 space-x-2">
-                  <Show
-                    when={(platformStore.version || "UNKNOWN").includes("*")}
-                    fallback={<span class="text-primary">REL</span>}
-                  >
-                    <span class="text-warning">DEV</span>
-                  </Show>
-                  <span>{(platformStore.version || "UNKNOWN").replace("*", "")}</span>
-                </p>
+          <OverlayScrollbarsComponent
+            options={{
+              scrollbars: {
+                theme: `os-theme-${fullTheme()}`,
+                autoHide: "scroll",
+              },
+            }}
+            class="relative w-full h-full print:h-auto print:overflow-auto"
+            defer
+          >
+            <div class=" w-full flex flex-col items-center justify-center min-h-full">
+              <div class="max-w-3xl w-full flex flex-col items-center justify-center space-y-6">
+                <div class="flex flex-row space-x-6 items-center">
+                  <LogoAnimate width={80} height={80} />
+                  <div class="flex flex-col space-y-1">
+                    <h2 class="text-3xl font-bold flex flex-row">
+                      <span class="text-primary">R</span>
+                      <span class="opacity-80">et</span>
+                      <span class="opacity-60">&nbsp;2&nbsp;</span>
+                      <span class="text-error">S</span>
+                      <span class="opacity-80">hell</span>
+                    </h2>
+                    <p class="text-base font-bold opacity-60 space-x-2">
+                      <Show
+                        when={(platformStore.version || "UNKNOWN").includes("*")}
+                        fallback={<span class="text-primary">REL</span>}
+                      >
+                        <span class="text-warning">DEV</span>
+                      </Show>
+                      <span>{(platformStore.version || "UNKNOWN").replace("*", "")}</span>
+                    </p>
+                  </div>
+                </div>
+                <Divider class="w-full" />
+                <h3 class="font-bold">Developers</h3>
+                <div class="flex flex-row flex-wrap justify-center">
+                  <Tag level="success" class="m-1"><a href="https://github.com/Reverier-Xu" target="_blank">Reverier-Xu</a></Tag>
+                </div>
+                <h3 class="font-bold">Contributors</h3>
+                <div class="flex flex-row flex-wrap justify-center">
+                  <Tag level="info" class="m-1"><a href="https://github.com/cdcq" target="_blank">cdcq</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://github.com/orangecheers-x" target="_blank">Orange Cheers</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://github.com/frankli0324" target="_blank">frankli0324</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://github.com/DX39061" target="_blank">DX3906</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://github.com/GZTimeWalker" target="_blank">GZTime</a></Tag>
+                </div>
+                <h3 class="font-bold">Artists</h3>
+                <div class="flex flex-row flex-wrap justify-center">
+                  <Tag level="info" class="m-1"><a href="https://github.com/hypnoticss" target="_blank">Hypnotics</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://twitter.com/LAttic1ng" target="_blank">Ac4ae0</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://github.com/arttnba3" target="_blank">Arttnba3</a></Tag>
+                  <Tag level="info" class="m-1"><span>W3nL0u</span></Tag>
+                </div>
+                <h3 class="font-bold">Opensource projects & 3rd libraries</h3>
+                <div class="flex flex-row flex-wrap justify-center">
+                  <Tag level="warning" class="m-1"><a href="https://github.com/rust-lang/rust" target="_blank">Rust</a></Tag>
+                  <Tag level="warning" class="m-1"><a href="https://www.typescriptlang.org/" target="_blank">Typescript</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://tokio.rs/" target="_blank">Tokio</a></Tag>
+                  <Tag level="info" class="m-1"><a href="https://solidjs.com/" target="_blank">Solid JS</a></Tag>
+                  <Tag level="success" class="m-1"><a href="https://k3s.io/" target="_blank">K3S</a></Tag>
+                  <Tag level="success" class="m-1"><a href="https://www.postgresql.org/" target="_blank">PostgreSQL</a></Tag>
+                  <Tag level="success" class="m-1"><a href="https://valkey.io/" target="_blank">Valkey</a></Tag>
+                  <Tag level="success" class="m-1"><a href="https://git-scm.com/" target="_blank">Git</a></Tag>
+                  <Tag level="success" class="m-1"><a href="https://rune-rs.github.io/" target="_blank">Rune</a></Tag>
+                </div>
               </div>
             </div>
-            <Divider class="w-full" />
-            <h3 class="font-bold">Developers</h3>
-            <div class="flex flex-row flex-wrap justify-center">
-              <Tag level="success" class="m-1"><a href="https://github.com/Reverier-Xu" target="_blank">Reverier-Xu</a></Tag>
-            </div>
-            <h3 class="font-bold">Contributors</h3>
-            <div class="flex flex-row flex-wrap justify-center">
-              <Tag level="info" class="m-1"><a href="https://github.com/cdcq" target="_blank">cdcq</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://github.com/orangecheers-x" target="_blank">Orange Cheers</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://github.com/frankli0324" target="_blank">frankli0324</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://github.com/DX39061" target="_blank">DX3906</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://github.com/GZTimeWalker" target="_blank">GZTime</a></Tag>
-            </div>
-            <h3 class="font-bold">Artists</h3>
-            <div class="flex flex-row flex-wrap justify-center">
-              <Tag level="info" class="m-1"><a href="https://github.com/hypnoticss" target="_blank">Hypnotics</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://twitter.com/LAttic1ng" target="_blank">Ac4ae0</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://github.com/arttnba3" target="_blank">Arttnba3</a></Tag>
-              <Tag level="info" class="m-1"><span>W3nL0u</span></Tag>
-            </div>
-            <h3 class="font-bold">Opensource projects & 3rd libraries</h3>
-            <div class="flex flex-row flex-wrap justify-center">
-              <Tag level="warning" class="m-1"><a href="https://github.com/rust-lang/rust" target="_blank">Rust</a></Tag>
-              <Tag level="warning" class="m-1"><a href="https://www.typescriptlang.org/" target="_blank">Typescript</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://tokio.rs/" target="_blank">Tokio</a></Tag>
-              <Tag level="info" class="m-1"><a href="https://solidjs.com/" target="_blank">Solid JS</a></Tag>
-              <Tag level="success" class="m-1"><a href="https://k3s.io/" target="_blank">K3S</a></Tag>
-              <Tag level="success" class="m-1"><a href="https://www.postgresql.org/" target="_blank">PostgreSQL</a></Tag>
-              <Tag level="success" class="m-1"><a href="https://valkey.io/" target="_blank">Valkey</a></Tag>
-              <Tag level="success" class="m-1"><a href="https://git-scm.com/" target="_blank">Git</a></Tag>
-              <Tag level="success" class="m-1"><a href="https://rune-rs.github.io/" target="_blank">Rune</a></Tag>
-            </div>
-          </div>
+          </OverlayScrollbarsComponent>
         </section>
         <section class="h-full min-h-full snap-center flex flex-col items-center justify-center relative space-y-8 p-3">
           <Reverier width={256} height={256} class="" />
