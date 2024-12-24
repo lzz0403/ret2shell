6f09c836dfb75038c887cf510d0987a9a3469537
Reverier-Xu <reverier.xu@woooo.tech>
2024-12-24 17:47:00 +0800

    :bug: fix database transaction (#78)

--- crates/server/src/routes/game/worker.rs
+++ crates/server/src/routes/game/worker.rs
@@ -362,7 +362,6 @@ async fn submission_worker_exec(
       })),
     };
     queue.publish("event", event).await.ok(); // publish scoreboard update event if nessary
-    txn.commit().await?;
     if changed {
       queue.publish("scoreboard", challenge.clone()).await.ok();
       cache.at("challenge").del(challenge.id).await.ok();
@@ -381,6 +380,7 @@ async fn submission_worker_exec(
       game.name
     );
   }
+  txn.commit().await?;
 
   let txn = db.conn.begin().await?;
   // stage 4: create audit if necessary
