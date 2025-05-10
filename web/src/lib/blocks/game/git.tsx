40988e07c03085956daeb66dd4a64aeb7c3a7f55
Reverier-Xu <reverier.xu@woooo.tech>
2025-05-11 01:49:05 +0800

    :art: add loading for git entry

--- web/src/lib/blocks/game/git.tsx
+++ web/src/lib/blocks/game/git.tsx
@@ -15,11 +15,16 @@ import { DateTime } from "luxon";
 import { For, Show, createEffect, createSignal, untrack } from "solid-js";
 
 export default function () {
-  const [repoName, setRepoName] = createSignal<string>(gameStore.current?.name || "");
+  const [repoName, setRepoName] = createSignal<string>(
+    gameStore.current?.name || "",
+  );
   const [loading, setLoading] = createSignal(false);
 
   const [searchParams] = useSearchParams();
-  const path = () => ((searchParams.path ?? "") as string).trim().replace(/^[\/]+|[\/]+$/g, "") || ".";
+  const path = () =>
+    ((searchParams.path ?? "") as string)
+      .trim()
+      .replace(/^[\/]+|[\/]+$/g, "") || ".";
   const [objects, setObjects] = createSignal<ObjectInfo[]>([]);
 
   createEffect(() => {
@@ -42,7 +47,7 @@ export default function () {
                 return a.path.localeCompare(b.path);
               }
               return b.type.localeCompare(a.type);
-            })
+            }),
           );
         } catch (err) {
           handleHttpError(err as Error, t("game.git.errors.fetchRepo.title")!);
@@ -58,11 +63,16 @@ export default function () {
         <span class="icon-[fluent--branch-fork-20-regular] w-5 h-5" />
         <span>{t("game.git.title")}</span>
       </h3>
-      <Card level="info" contentClass="p-2 flex flex-row space-x-2 items-center">
+      <Card
+        level="info"
+        contentClass="p-2 flex flex-row space-x-2 items-center"
+      >
         <span class="icon-[fluent--info-20-regular] w-5 h-5 shrink-0" />
         <span>{t("game.git.cloneTip")}</span>
       </Card>
-      <Clipboard value={`${window.location.origin}/api/game/${gameStore.current?.id}/repo/${repoName()}.git`} />
+      <Clipboard
+        value={`${window.location.origin}/api/game/${gameStore.current?.id}/repo/${repoName()}.git`}
+      />
       <h3 class="h-12 flex items-center border-b border-b-layer-content/10 font-bold space-x-2">
         <span class="icon-[fluent--branch-fork-20-regular] w-5 h-5" />
         <A class="font-bold" href={`/games/${gameStore.current?.id}/admin/git`}>
@@ -84,7 +94,7 @@ export default function () {
         </For>
         <div class="flex-1" />
         <Show when={loading()}>
-          <Spin />
+          <Spin width={20} height={20} />
           <span>{t("general.loading.short")}</span>
         </Show>
       </h3>
@@ -96,21 +106,39 @@ export default function () {
                 ghost
                 justify="start"
                 href={`/games/${gameStore.current?.id}/admin/git?path=${object.path}`}
-                class="overflow-hidden relative"
+                class={clsx(
+                  "overflow-hidden relative",
+                  loading() && object.path === path() && "!bg-layer-content/5",
+                )}
                 disabled={loading() || object.type === "blob"}
                 title={object.subject || ""}
               >
-                <span
-                  class={clsx(
-                    object.type === "blob" ? "icon-[fluent--document-20-regular]" : "icon-[fluent--folder-20-regular]",
-                    "w-5 h-5"
-                  )}
-                />
-                <span class="flex-1 text-start truncate font-normal">{object.path.split("/").slice(-1)[0]}</span>
+                <Show
+                  when={loading() && object.path === path()}
+                  fallback={
+                    <span
+                      class={clsx(
+                        object.type === "blob"
+                          ? "icon-[fluent--document-20-regular]"
+                          : "icon-[fluent--folder-20-regular]",
+                        "w-5 h-5",
+                      )}
+                    />
+                  }
+                >
+                  <Spin width={20} height={20} />
+                </Show>
+                <span class="flex-1 text-start truncate font-normal">
+                  {object.path.split("/").slice(-1)[0]}
+                </span>
                 <span class="text-primary opacity-60">{object.commit}</span>
-                <span class="flex-1 text-start truncate opacity-60 font-normal">{object.subject}</span>
+                <span class="flex-1 text-start truncate opacity-60 font-normal">
+                  {object.subject}
+                </span>
                 <span class="opacity-60 font-normal">
-                  {DateTime.fromSeconds(object.last_modified || 0).toFormat("yyyy-MM-dd HH:mm")}
+                  {DateTime.fromSeconds(object.last_modified || 0).toFormat(
+                    "yyyy-MM-dd HH:mm",
+                  )}
                 </span>
               </Link>
               <Divider class="w-full" />
